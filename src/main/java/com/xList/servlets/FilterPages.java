3cf87fe24937a452cd92ec351192cff5b57a7de6
Vlad Solovii <One.Red.Wlad@gmail.com>
2017-08-22 17:45:55 +0300

    Fixed logout button and seach form

--- src/main/java/com/xList/servlets/FilterPages.java
+++ src/main/java/com/xList/servlets/FilterPages.java
@@ -29,6 +29,7 @@ public class FilterPages implements Filter{
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
 
+        String logOutBtn = IndexHtmlView.getInstance().getLogoutButton();
         String path = ((HttpServletRequest) request).getRequestURI();
         logger.fine("path if\t"+path);
         if (path.startsWith("/noteajax/")){
@@ -44,8 +45,8 @@ public class FilterPages implements Filter{
             }
 
             if (session.getAttribute("username") !=null){
-                IndexTemplate indexTemplate = new IndexTemplate(printWriter);
-                top = top.replace("<!-- servletInsert01 -->",indexTemplate.getLoggedUserBar(request));
+                logOutBtn = logOutBtn.replace("<!--username-->", session.getAttribute("username").toString());
+                top = top.replace("<!-- servletInsert01 -->",logOutBtn);
             }
             printWriter.write(top);
             chain.doFilter(request,response);
--- src/main/java/com/xList/views/IndexHtmlView.java
+++ src/main/java/com/xList/views/IndexHtmlView.java
@@ -7,6 +7,7 @@ public class IndexHtmlView {
     private final String topHtml;
     private final String bottomHtml;
     private final String loginForm;
+    private final String logoutButton;
     private final String registerForm;
     private final String rightBar;
 
@@ -23,6 +24,7 @@ public class IndexHtmlView {
         this.loginForm=pathHtml.getPartialHtml("login-form.html");
         this.topHtml=pathHtml.getPartialHtml("top.html");
         this.bottomHtml=pathHtml.getPartialHtml("bottom.html");
+        this.logoutButton=pathHtml.getPartialHtml("logout-button.html");
         this.registerForm=pathHtml.getPartialHtml("register-form.html");
         this.rightBar = pathHtml.getPartialHtml("right-bat.html");
     }
@@ -39,6 +41,9 @@ public class IndexHtmlView {
         return loginForm;
     }
 
+    public String getLogoutButton() {
+        return logoutButton;
+        }
 
     public String getRegisterForm() {
         return registerForm;
